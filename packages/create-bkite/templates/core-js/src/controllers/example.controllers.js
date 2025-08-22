cf2de92154dfeb8a21d38f62cbf91981e46be84a
AnilKumt <anilkumt51@gmail.com>
2025-08-22 18:58:55 +0530

    changed the global error handler and example handler making use of http codes constants

--- packages/create-bkite/templates/core-js/src/controllers/example.controllers.js
+++ packages/create-bkite/templates/core-js/src/controllers/example.controllers.js
@@ -1,9 +1,9 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { ApiError } from "../utils/ApiError.js";
-import Http
+import HttpStatus from "../constants/httpStatusCodes.js";
 
 export const getExample = asyncHandler(
     (req, res) => {
-        res.status(200).json(new ApiResponse(200, {}, "Example route fetched successfully"));
+        res.status(HttpStatus.OK).json(new ApiResponse(HttpStatus.OK, {}, "Example route fetched successfully"));
     });
