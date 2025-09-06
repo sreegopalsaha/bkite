22c178f1e481d694ba17f5701c3861140bac1cb3
AnilKumt <anilkumt51@gmail.com>
2025-09-06 10:30:40 +0530

    example routesa added and tested

---
+++ packages/create-bkite/templates/mongodb-mongoose-ts/src/routes/example.routes.ts
@@ -0,0 +1,8 @@
+import express, { Router } from "express";
+import { getExample } from "../controllers/example.controllers";
+
+const router: Router = express.Router();
+
+router.get("/", getExample);
+
+export default router;
